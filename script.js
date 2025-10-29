9461fa39b1ea589a909af6c98dd297904be4811c
Sutan Landung Ayunan <suzushi23@gmail.com>
2025-10-29 14:24:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Efek fade-in saat halaman dimuat
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => {
+    document.body.style.transition = "opacity 1.2s ease";
+    document.body.style.opacity = 1;
+  }, 100);
+});
